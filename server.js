203b94898f725a94c6e1ccc229852fe1264d2a31
andrewhodel <andrewhodel@gmail.com>
2013-01-24 12:40:11 -0600

    interface improvements, mikrotik simple client improvements, added sta/master wirelessMode config option

--- server.js
+++ server.js
@@ -1085,6 +1085,7 @@ name* - STR host display name
 latitude - FLOAT
 longitude - FLOAT
 notes* - STR notes about the host
+wirelessMode - STR wireless mode, ap/sta
 channel - INT wifi channel for host
 vlan - INT vlan ID for host
 ssid - STR ssid for host
@@ -1136,7 +1137,7 @@ router.post('/host').bind(function (req, res, params) {
                 } else {
                     zoneId = String(results[2].zoneId);
                     db.collection('hosts', function (err, collection) {
-                        collection.insert({'login':params.login, 'key':params.key, 'name':params.name, 'latitude':params.latitude, 'longitude':params.longitude, 'notes':params.notes, 'channel':params.channel, 'vlan':params.vlan, 'ssid':params.ssid, 'encryption':params.encryption, 'encryptionKey':params.encryptionKey, 'groupId':new mongodb.ObjectID(params.groupId), 'zoneId':new mongodb.ObjectID(zoneId), 'reboot':1, 'createdAt':Math.round((new Date()).getTime() / 1000)}, function(err, docs) {
+                        collection.insert({'login':params.login, 'key':params.key, 'name':params.name, 'latitude':params.latitude, 'longitude':params.longitude, 'notes':params.notes, 'wirelessMode':params.wirelessMode, 'channel':params.channel, 'vlan':params.vlan, 'ssid':params.ssid, 'encryption':params.encryption, 'encryptionKey':params.encryptionKey, 'groupId':new mongodb.ObjectID(params.groupId), 'zoneId':new mongodb.ObjectID(zoneId), 'reboot':1, 'createdAt':Math.round((new Date()).getTime() / 1000)}, function(err, docs) {
                             if (err) {
                                 res.send(500, {}, {'error':err});
                             } else {
@@ -1163,6 +1164,7 @@ name - STR host display name
 latitude - FLOAT
 longitude - FLOAT
 notes - STR notes about the host
+wirelessMode - STR wireless mode, ap/sta
 channel - INT wifi channel for host
 vlan - INT vlan ID for host
 ssid - STR ssid for host
@@ -1178,6 +1180,7 @@ RESPONSE CODES
 */
 router.put('/host').bind(function (req, res, params) {
 	auth(params.username, params.password, true, req, res, function (err, docs) {
+            console.log(params);
 
             async.series([
                 function(callback) {
@@ -1223,6 +1226,10 @@ router.put('/host').bind(function (req, res, params) {
 
                         if (params.notes != undefined && params.notes != '') {
                             i.notes = params.notes;
+                        }
+
+                        if (params.wirelessMode != undefined && params.wirelessMode != '') {
+                            i.wirelessMode = params.wirelessMode;
                         }
 
                         if (params.channel != undefined && params.channel != '') {
